a7231522b5d680b6bb04420b04dfb40f0e1564a0
Abs Dev <85418905+AbsApps@users.noreply.github.com>
2021-11-15 17:46:38 -0600

    index upgrade

--- js/commons.js
+++ js/commons.js
@@ -1,38 +1,38 @@
 var commons = {};
 (function () {
-  var that = this;
+	var that = this;
 
-  this.init = function () {
-    console.log("JS is ready");
-    that.initSliderTeam();
-  };
+	this.init = function () {
+		console.log("JS is ready");
+		that.initSliderTeam();
+	};
 
-  /**
-   * Escucha cuando se cmabiam las categorias
-   */
-  this.addListener = function () {};
+	/**
+	 * Escucha cuando se cmabiam las categorias
+	 */
+	this.addListener = function () { };
 
-  /**
-   * Este meodo ini
-   */
-  this.initSliderTeam = function () {
-    if ($(".team-caurousel")) {
-      $(".team-caurousel").slick({
-        dots: false,
-        arrows: false,
-        infinite: false,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      });
-    }
-  };
+	/**
+	 * Este meodo ini
+	 */
+	this.initSliderTeam = function () {
+		if ($(".team-caurousel")) {
+			$(".team-caurousel").slick({
+				dots: false,
+				arrows: false,
+				infinite: false,
+				autoplay: true,
+				autoplaySpeed: 2000,
+				speed: 300,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			});
+		}
+	};
 
-  
 
-  window.onload = function (e) {
-    that.init();
-  };
+
+	window.onload = function (e) {
+		that.init();
+	};
 }.call(commons));
